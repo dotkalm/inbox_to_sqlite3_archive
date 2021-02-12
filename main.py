e1d1864edc5efd7a625004c0de554e74ff5307dc
joel holmberg <joelmholmberg@gmail.com>
2021-02-12 15:35:50 -0800

    config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from actions.gmail_backup import gmail_archive_and_expunge
 from dotenv import load_dotenv
 load_dotenv()
 from models import c
+from actions.firebase import upload_blob
 
 email = os.getenv("GMAIL_ADDRESS")
 
@@ -17,6 +18,11 @@ def recursive_retrieve():
         c.close()
         print('done????')
         print(len(all_ids), 'ids length')
-recursive_retrieve()
 
-
+def upload_sqlite_blob(file_name):
+    file_path = os.path.join(os.getcwd(), file_name)
+    print(file_path)
+    uploaded_file = upload_blob(file_path, 'db/' + file_name)
+    print(uploaded_file)
+    
+upload_sqlite_blob('gmail_inbox.sqlite3')
